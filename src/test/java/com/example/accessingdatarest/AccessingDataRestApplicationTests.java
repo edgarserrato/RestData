f0836ba720a19c99e361c440b6d7d27ceb63a133
edgarserrato <edgar.serrato33@gmail.com>
2024-06-14 16:12:53 -0600

    Initial commit

---
+++ src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.accessingdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class AccessingDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
+
+@SpringBootTest
+class AccessingDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
+
+
+
+
